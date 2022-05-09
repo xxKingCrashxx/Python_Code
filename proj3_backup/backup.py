6032f3424bfe54e9e24cef6687c0578fd7b248e9
Austin Decker <deckaj01@pfw.edu>
2022-05-09 11:52:55 -0400

    added proj3 backup script

---
+++ proj3_backup/backup.py
@@ -0,0 +1,18 @@
+from zipfile import ZipFile
+from datetime import date
+from os import os.path
+
+#variables that represent the src directory and destination directory.
+#change variables as needed.
+
+SRC_DIR = ""
+DEST_DIR = ""
+
+def main():
+
+
+
+if __name__ == '__main__':
+	main()
+
+
