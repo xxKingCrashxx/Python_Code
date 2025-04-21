12de226c2a24716395bcc3bfdb10442b3f046b6e
xxking_crashxx <ajdecker2@gmail.com>
2025-04-20 21:34:28 -0400

    started implementation of rcon to remove library dependency.

---
+++ proj9_advanced_minecraft_server_backup/advanced_minecraft_backup/mcrcon.py
@@ -0,0 +1,24 @@
+
+
+class MCRcon:
+    def __init__(self, server_ip, rcon_passwd, rcon_port=25575, ):
+        self.__server_ip = server_ip
+        self.__rcon_passwd = rcon_passwd
+        self.__rcon_port = rcon_port
+        self.__sock = None
+        self.__req_num = 0
+
+    def send_command(command_string):
+        pass
+
+    def close_connection():
+        pass
+
+    def __login():
+        pass
+
+    def __recv_packet():
+        pass
+
+    def __send_packet():
+        pass
