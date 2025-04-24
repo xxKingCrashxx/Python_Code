c9c8096754450594da3404264bcd38054102d99c
xxking_crashxx <ajdecker2@gmail.com>
2025-04-24 15:35:01 -0400

    modified script  to work with timeseries collection instead.

--- proj11_server_scanner/server_scanner.py
+++ proj11_server_scanner/server_scanner.py
@@ -40,10 +40,10 @@ def player_exists(player_id):
             return True
     return False
     
-def log_event(eventType, player, timestamp_string ):
+def log_event(eventType, player, timestamp ):
 
-    events = db.get_collection("player_events")
-    player_sessions = db.get_collection("player_sessions")
+    events = db.get_collection("player_events_timeseries")
+    player_sessions = db.get_collection("player_sessions_timeseries")
     players = db.get_collection("Players")
 
     if eventType == EVENT_TYPE["NEW_PLAYER"]:
@@ -55,7 +55,7 @@ def log_event(eventType, player, timestamp_string ):
         players.insert_one({
             "_id": str(player["player"].id),
             "player_name": player["player"].name,
-            "first_joined": timestamp_string,
+            "first_joined": timestamp,
             "play_time": 0,
         })
 
@@ -63,28 +63,34 @@ def log_event(eventType, player, timestamp_string ):
         pass
 
     elif eventType == EVENT_TYPE["PLAYER_LEAVE"]:
-        play_time_minutes = round(calculate_playtime(player["joined_at"], timestamp_string))
+        play_time_minutes = round(calculate_playtime(player["joined_at"].isoformat(), timestamp.isoformat()))
         players.update_one(
             {"_id": str(player["player"].id)},
             {
                 "$inc": {"play_time": play_time_minutes}, 
-                "$set": {"last_seen":timestamp_string}
+                "$set": {"last_seen":timestamp}
             }
         )
         player_sessions.insert_one({
-            "play_time": play_time_minutes,
-            "player": {"player_id": player["player"].id, 
-                       "player_name": player["player"].name},
-            "join_timestamp": player["joined_at"],
-            "left_timestamp": timestamp_string
+            "session_info": {
+                "play_time": play_time_minutes,
+                "player": {
+                    "player_id": player["player"].id, 
+                    "player_name": player["player"].name
+                },
+                "left_timestamp": timestamp
+            },
+            "join_timestamp": player["joined_at"], 
         })
 
     # regardless of what event it is, log it into the database. 
     events.insert_one({
-            "player_name": player["player"].name,
-            "player_id": str(player["player"].id),
-            "event_type": EVENT_TYPE_REV_MAP[eventType],
-            "timestamp": timestamp_string,
+            "timestamp": timestamp,
+            "event_info": {
+                "player_name": player["player"].name,
+                "player_id": str(player["player"].id),
+                "event_type": EVENT_TYPE_REV_MAP[eventType],
+            }
     })
 
     
@@ -96,7 +102,7 @@ def main():
         try:
             server = JavaServer.lookup(address="184.16.77.172:25565", timeout=10)
             status = server.status()
-            current_time = datetime.now().isoformat()
+            current_time = datetime.now()
 
             current_sample = status.players.sample or []
             current_players_online = set(p.name for p in current_sample)
@@ -105,7 +111,7 @@ def main():
             left_now = last_players_online - current_players_online
 
             if joined_now or left_now:
-                print(f"[{current_time}] Server IP: {server.address}\tPlayers Online: {status.players.online}")
+                print(f"[{current_time.isoformat()}] Server IP: {server.address}\tPlayers Online: {status.players.online}")
                 print("Players:", [p.name for p in current_sample])
 
             for player in current_sample:
@@ -125,7 +131,7 @@ def main():
             for name in left_now:
                 player_map[name]["left_at"] = current_time
                 log_event(EVENT_TYPE["PLAYER_LEAVE"], player_map[name], current_time)
-                print(f"[{current_time}] {name} left the server.")
+                print(f"[{current_time.isoformat()}] {name} left the server.")
 
             last_players_online = current_players_online
         except Exception as e:
